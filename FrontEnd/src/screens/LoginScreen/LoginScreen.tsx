aefea09baa5df80b5205482f830d2dc2a50213ce
노현종 <74768098+manOfBackend@users.noreply.github.com>
2022-01-31 01:39:45 +0900

    [#4, #8] 로그인 폼에 로고 추가, 스크롤바 적용

--- FrontEnd/src/screens/LoginScreen/LoginScreen.tsx
+++ FrontEnd/src/screens/LoginScreen/LoginScreen.tsx
@@ -15,11 +15,12 @@ import Typography from '@mui/material/Typography';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
+import Logo from 'src/assets/logo';
 import { LoginIcon } from 'src/components/@Icons';
+import { ScrollPageWrapper } from 'src/components/@styled/layout';
 import KakaoLogin from 'src/components/SocialLogin/KakaoLogin';
 import { LoginData } from 'src/model/model';
 import { loginUserAction } from 'src/redux/authentication/authenticationActions';
-
 type State = {
   email: string;
   password: string;
@@ -75,81 +76,78 @@ const LoginScreen = () => {
     dispatch(loginUserAction({ email, password }));
   };
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          로그인
-        </Typography>
-        <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            placeholder={'everyone@everyone.com'}
-            label="이메일"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="비밀번호"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="이 기기에서 로그인 유지"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            size="large"
-            startIcon={<LoginIcon />}
-          >
-            로그인
-          </Button>
-          <KakaoLogin />
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                비밀번호 찾기
-              </Link>
+    <ScrollPageWrapper>
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Logo width="300px" height="150px" />
+          <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              placeholder={'everyone@everyone.com'}
+              label="이메일"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="비밀번호"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="이 기기에서 로그인 유지"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              size="large"
+              startIcon={<LoginIcon />}
+            >
+              로그인
+            </Button>
+            <KakaoLogin />
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  비밀번호 찾기
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  회원 가입
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                회원 가입
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </ScrollPageWrapper>
   );
 };
 
