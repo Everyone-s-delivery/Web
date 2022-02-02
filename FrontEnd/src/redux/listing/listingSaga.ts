eb9952dfd16969f178f1308d0c25efab4811f787
노현종 <74768098+manOfBackend@users.noreply.github.com>
2022-02-02 23:44:54 +0900

    [#1] Posts 컴포넌트 추가, styled 작업

--- FrontEnd/src/redux/listing/listingSaga.ts
+++ FrontEnd/src/redux/listing/listingSaga.ts
@@ -1,24 +1,20 @@
 import { call, fork, put, takeLatest } from 'redux-saga/effects';
 
 import { ActionType } from 'src/model/model';
+import { usePostsQuery } from 'src/services/queries/posts';
 import { createApiCall, listingRoute, MethodType } from '../../services/Api';
 
 // login
-function* getListingSaga({ payload }: { payload?: string }): Generator<any> {
+function* getListingSaga(): Generator<any> {
   try {
-    const response: any = yield call(createApiCall, {
-      method: MethodType.GET,
-      url: `${listingRoute}${payload ? `?timestamp=${payload}` : ''}`,
-      data: undefined,
-      auth: true,
-    });
+    const response: any = yield call(usePostsQuery);
     if (response.status === 'ok') {
       yield put({
         type: ActionType.LISTING_REQUEST_SUCCESS,
-        payload: response.data.saleListings,
+        payload: response,
       });
     } else {
-      yield put({ type: ActionType.LOGIN_USER_ERROR, payload: 'error' });
+      yield put({ type: ActionType.LISTING_REQUEST_ERROR, payload: response.status });
     }
   } catch (error) {
     yield put({ type: ActionType.LISTING_REQUEST_ERROR, payload: error });
---
+++ FrontEnd/src/screens/Posts/Posts.tsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+
+import { getListingAction } from 'src/redux/listing/listingActions';
+import { ScrollPageWrapper } from '../../components/@styled/layout';
+import { QUERY } from '../../constants/queries';
+import { Container, ContentWrapper } from './Posts.style';
+
+const Posts = () => {
+  const dispatch = useDispatch();
+  dispatch(getListingAction());
+
+  return (
+    <ScrollPageWrapper>
+      <Container>
+        <ContentWrapper>
+          <PostList
+            users={results}
+            isFetchingNextPage={isFetchingNextPage}
+            onIntersect={handleIntersect}
+            queryKey={[QUERY.GET_POSTS, { username }]}
+          />
+        </ContentWrapper>
+      </Container>
+    </ScrollPageWrapper>
+  );
+};
+
+export default Posts;
