1d35eabd46dd33a8bf0d7016545bd161b8d3c01b
hjman <74768098+manOfBackend@users.noreply.github.com>
2022-02-14 00:15:53 +0900

    🔧 [#34] Chore: 추가 storybook preview.js, main.js

--- FrontEnd/.storybook/main.js
+++ FrontEnd/.storybook/main.js
@@ -1,3 +1,4 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -8,5 +9,14 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
 }
