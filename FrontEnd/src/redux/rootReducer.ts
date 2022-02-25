e0d95e563498aea7f3c731fb5c67d14629e9962f
rnjstjdgh <gshgsh0831@gmail.com>
2022-02-25 16:29:10 +0900

    #20:[bug]:충돌 해결

--- FrontEnd/src/redux/rootReducer.ts
+++ FrontEnd/src/redux/rootReducer.ts
@@ -1,35 +1,8 @@
 import { routerReducer, RouterState } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
-<<<<<<< HEAD
 import { AuthenticationReducerType } from './login/authenticationReducer';
 import { loginReducer } from './login/loginSlice';
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {
-  authenticationReducer,
-  AuthenticationReducerType,
-} from './authentication/authenticationReducer';
-=======
-import { AuthenticationReducerType } from './login/authenticationReducer';
-import { loginReducer } from './login/loginSlice';
->>>>>>> dev
-=======
-import { AuthenticationReducerType } from './login/authenticationReducer';
-import { loginReducer } from './login/loginSlice';
->>>>>>> dev
-=======
-import { AuthenticationReducerType } from './login/authenticationReducer';
-import { loginReducer } from './login/loginSlice';
->>>>>>> dev
-=======
-import { AuthenticationReducerType } from './login/authenticationReducer';
-import { loginReducer } from './login/loginSlice';
->>>>>>> dev
->>>>>>> 7f319b30
 import { postsReducer, PostsReducerType } from './posts/postsReducer';
 import { signupReducer, SignupReducerType } from './signup/signupReducer';
 
@@ -41,27 +14,7 @@ export interface RootState {
 }
 const rootReducer = combineReducers({
   posts: postsReducer,
-<<<<<<< HEAD
   loginForm: loginReducer,
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  loginForm: authenticationReducer,
-=======
-  loginForm: loginReducer,
->>>>>>> dev
-=======
-  loginForm: loginReducer,
->>>>>>> dev
-=======
-  loginForm: loginReducer,
->>>>>>> dev
-=======
-  loginForm: loginReducer,
->>>>>>> dev
->>>>>>> 7f319b30
   signup: signupReducer,
   routerReducer,
 });
