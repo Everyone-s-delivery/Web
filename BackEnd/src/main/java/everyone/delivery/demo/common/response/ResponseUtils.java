8698586d97722221da21cb1e6faa24d9c0139782
rnjstjdgh <gshgsh0831@daum.net>
2022-03-30 23:09:40 +0900

    [#30]:Feat:글 목록 페이징 다음 페이지 존재 유무를 서버에서 판단하도록 수정

--- BackEnd/src/main/java/everyone/delivery/demo/common/response/ResponseUtils.java
+++ BackEnd/src/main/java/everyone/delivery/demo/common/response/ResponseUtils.java
@@ -1,7 +1,11 @@
 package everyone.delivery.demo.common.response;
 
 import everyone.delivery.demo.common.exception.error.RestError;
+import everyone.delivery.demo.common.request.dto.PagingRequestDto;
+import everyone.delivery.demo.common.response.dto.PagingResponseDto;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Collection;
 
 public class ResponseUtils {
 
@@ -13,6 +17,26 @@ public class ResponseUtils {
      */
     public static <T> ResponseEntity out(T data){
         return out(convertSuccessResponse(data));
+    }
+
+    /***
+     * >  Slice 형태의 페이징에서 다음 페이지 유무를 서버에서 판단해서 내려주기 위한 메소드
+     * >  api 의 직관성을 위해 이렇게 구현하는 것이 좋다고 결정해서 구현 함
+     * @param pagingData
+     * @param pagingRequestDto
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseEntity out(Collection pagingData, PagingRequestDto pagingRequestDto){
+        PagingResponseDto result;
+        if(pagingData.size() == pagingRequestDto.getLimit() + 1){
+            pagingData.remove(pagingData.toArray()[pagingData.size()-1]);
+            result = PagingResponseDto.builder().next(true).data(pagingData).build();
+        }
+        else{
+            result = PagingResponseDto.builder().next(false).data(pagingData).build();
+        }
+        return out(convertSuccessResponse(result));
     }
 
     /***
