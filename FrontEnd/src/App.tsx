675e8611426169799fe0a6386d7d93b41672f06c
hyunbell <man.of.backend@gmail.com>
2022-04-21 14:22:19 +0900

    [#1] feat: posts 라우팅 추가

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './App.style';
 import Footer from './components/Footer';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
+import Posts from './screens/Posts/Posts';
 import SignupScreen from './screens/Signup/Signup';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LoginScreen />}></Route>
           <Route path="/login" element={<LoginScreen />}></Route>
+          <Route path="/posts" element={<Posts />}></Route>
           <Route path="/signup" element={<SignupScreen />}></Route>
         </Routes>
         <Footer />
