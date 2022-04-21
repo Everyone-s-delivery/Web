9968769212ff259de96ea24879d5e41eacc9e6d4
hyunbell <man.of.backend@gmail.com>
2022-04-21 14:24:24 +0900

    [#1] fix: Post interface를 api와 동기화

--- FrontEnd/src/@types/index.ts
+++ FrontEnd/src/@types/index.ts
@@ -1,12 +1,20 @@
 import { API_ERROR_MESSAGE } from 'src/constants/messages';
 
 export interface Post {
-  id: number;
-  content: string;
-  authorName: string;
-  createdAt: string;
-  updatedAt: string;
-  comments: string[];
+  postId: number;
+  posterId: number;
+  posterEmail: string;
+  posterNickName: string;
+  title: string;
+  description: string;
+  addresses: string[];
+  regDate: string;
+  updateDate: string;
+}
+
+export interface PostsResponse {
+  next: boolean;
+  data: Post[];
 }
 
 export type APIErrorCode = keyof typeof API_ERROR_MESSAGE;
