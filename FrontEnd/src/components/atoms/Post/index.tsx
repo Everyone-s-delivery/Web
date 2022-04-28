7dee34bd4a940ac34ef6a4932fbca62b1ce0687d
hyunbell <man.of.backend@gmail.com>
2022-04-28 23:22:44 +0900

    [#1] refactor: Post 컴포넌트 디자인 개선 (feat. tailwind)

--- FrontEnd/src/components/atoms/Post/index.tsx
+++ FrontEnd/src/components/atoms/Post/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface PostProps {
+  id: string;
   imgSrc: string;
   title: string;
   content: string;
@@ -8,10 +9,10 @@ interface PostProps {
   price: string;
 }
 
-const Post = ({ imgSrc, title, content, region, price }: PostProps) => {
+const Post = ({ id, imgSrc, title, content, region, price }: PostProps) => {
   return (
     <article className="relative text-left block w-full divide-y divide-solid">
-      <a className="block no-underline text-black pl-56 py-6 pr-7 h-44">
+      <a className="block no-underline text-black pl-56 py-6 pr-7 h-44" href={`./posts/${id}`}>
         <div className="absolute left-7 right-7 bottom-7 top-7 h-44 w-44 overflow-hidden bg-white rounded-t-xl rounded-b-xl">
           <img className="w-full block align-middle" src={imgSrc} />
         </div>
