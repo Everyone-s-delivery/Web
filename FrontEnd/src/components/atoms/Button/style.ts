89f55a8862c98aecd6bcb86506d125454f96ff56
hyunbell <man.of.backend@gmail.com>
2022-05-10 13:17:34 +0900

    style: Button component 디자인 개선

--- FrontEnd/src/components/atoms/Button/style.ts
+++ FrontEnd/src/components/atoms/Button/style.ts
@@ -1,7 +1,7 @@
+import styled from '@emotion/styled';
 import { getColorValueFromCode } from '@src/constants';
-import styled from 'styled-components';
 
-import { getTextSizeFromCode } from './../../../constants/size';
+import { getTextSizeFromCode } from '../../../constants/size';
 import { ButtonProps } from '.';
 type ButtonStyleProps = Omit<ButtonProps, 'innerText' | 'handleClick'>;
 
@@ -17,15 +17,16 @@ const getHeightFromTextSize = (size: ButtonProps['textSize'] = 'medium'): string
 };
 
 export const ButtonStyle = styled.button<ButtonStyleProps>(
-  ({ buttonColor, textColor, textSize, hasBorder, hasShadow, hasFixedWidth, onHover }) => `
+  ({ buttonColor, textColor, textSize, hasBorder, hasShadow, width, hasFixedWidth, onHover }) => `
     border: 0rem;
     border-radius: 10rem;
     padding: 0rem 4rem;
-    font-size: ${getTextSizeFromCode(textSize)}
-    height: ${getHeightFromTextSize(textSize)}
+    font-size: ${getTextSizeFromCode(textSize)};
+    height: ${getHeightFromTextSize(textSize)};
     transition: 0.5s;
     font-weight: 400;
     line-height: normal;
+    width: ${width};
     &:hover {
         cursor: pointer;
         border-color: ${getColorValueFromCode(buttonColor)};
@@ -36,7 +37,20 @@ export const ButtonStyle = styled.button<ButtonStyleProps>(
     border-color ${getColorValueFromCode(textColor)};
     background-color: ${getColorValueFromCode(buttonColor)};
     font-size: ${textSize};
-
+    cursor: pointer;
+    align-items: center;
+    span {
+      display: inline-block;
+      font-size: 1rem;
+      align-items: center;
+      color: rgba(0, 0, 0, 0.85);
+      vertical-align: middle;
+      padding-bottom: 0.2rem;
+    }
+    svg {
+      height: 1.5rem;
+      margin-right: 0.75rem;
+    }
     ${
       hasBorder &&
       `
