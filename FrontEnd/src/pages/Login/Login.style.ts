d10089e38cc4797489a3775120512417c1473127
hyunbell <man.of.backend@gmail.com>
2022-05-10 13:22:26 +0900

    [#83] 자체 ui를 사용하여 개선한 Login Page 추가

--- FrontEnd/src/pages/Login/Login.style.ts
+++ FrontEnd/src/pages/Login/Login.style.ts
@@ -3,9 +3,11 @@ import { Page } from '@src/components/@styled/layout';
 
 export const Container = styled(Page)`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   position: fixed;
+  gap: 50px;
   padding-top: 0;
   width: 100%;
   height: 100%;
--- FrontEnd/src/pages/Login/Login.tsx
+++ FrontEnd/src/pages/Login/Login.tsx
@@ -1,39 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import TextField from '@mui/material/TextField';
 import Logo from '@src/assets/logo';
-import { LoginIcon } from '@src/components/@Icons';
-import { ScrollPageWrapper } from '@src/components/@styled/layout';
-import KakaoLogin from '@src/components/SocialLogin/KakaoLogin';
-import { LoginData } from '@src/model/model';
+import { KakaoIcon } from '@src/components/@Icons';
+import Button from '@src/components/atoms/Button';
+import TextField from '@src/components/atoms/TextField';
 import { loginActions } from '@src/redux/login/loginSlice';
 import { useRootState } from '@src/utils/useRootState';
-import { useFormik } from 'formik';
+import { Form, Formik, FormikProps } from 'formik';
 import * as Yup from 'yup';
 
 import { Container } from './Login.style';
 
-type State = {
+interface LoginFormValue {
   email: string;
   password: string;
-  loading: boolean;
-  message: string;
-};
+}
 
 const Login = () => {
-  const [state, setState] = useState<State>({
-    email: '',
-    loading: false,
-    password: '',
-    message: '',
-  });
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
@@ -54,106 +38,86 @@ const Login = () => {
         .email('유효한 이메일 형식을 입력하세요')
         .required('가입한 이메일 주소를 입력하세요.'),
       password: Yup.string()
-        .min(8, '비밀번호는 최소 8글자 이상입니다.')
+        .min(8, ({ min }) => `비밀번호는 최소 ${min}글자 이상입니다.`)
         .required('비밀번호를 입력하세요.'),
     });
   };
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values: LoginData) => {
-      handleLogin(values);
-    },
-  });
-
-  const handleLogin = (formValue: LoginData) => {
+  const handleLogin = (formValue: LoginFormValue) => {
     const { email, password } = formValue;
-
-    setState((prev) => ({
-      ...prev,
-      message: '',
-      loading: true,
-    }));
-
     dispatch(loginActions.login({ email, password }));
   };
+
   return (
-    <ScrollPageWrapper>
+    <div className="flex justify-center">
       <Container>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+        <Logo />
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values: LoginFormValue) => {
+            handleLogin(values);
           }}
         >
-          <Logo width="300px" height="150px" />
-          <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              placeholder={'everyone@everyone.com'}
-              label="이메일"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="비밀번호"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="이 기기에서 로그인 유지"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              size="large"
-              startIcon={<LoginIcon />}
-            >
-              로그인
-            </Button>
-            <KakaoLogin />
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  비밀번호 찾기
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  회원 가입
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
+          {({
+            errors,
+            touched,
+            handleSubmit,
+            handleChange,
+            values,
+          }: FormikProps<LoginFormValue>) => (
+            <Form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
+              <div className="flex flex-col mb-10">
+                <TextField
+                  required
+                  type={'email'}
+                  name="email"
+                  label="아이디"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+                {touched.email && errors.email && (
+                  <div className="text-red-900">{errors.email}</div>
+                )}
+                <TextField
+                  required
+                  type={'password'}
+                  name="password"
+                  label="비밀번호"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+                {touched.password && errors.password && (
+                  <div className="text-red-900">{errors.password}</div>
+                )}
+              </div>
+              <div className="flex flex-col gap-10">
+                <Button
+                  innerText="로그인"
+                  buttonColor={'olive'}
+                  textColor={'white'}
+                  hasFixedWidth
+                  hasShadow
+                  textSize="large"
+                />
+                <Button
+                  innerText="로그인"
+                  buttonColor={'cafe_yellow'}
+                  textColor={'black'}
+                  hasFixedWidth
+                  hasShadow
+                  textSize="large"
+                  icon={<KakaoIcon />}
+                />
+              </div>
+            </Form>
+          )}
+        </Formik>
       </Container>
-    </ScrollPageWrapper>
+    </div>
   );
 };
 
